153f295c992c1608bce1078f791f92459482af63
Sandesh Ghimire <sandeshghimire202@gmail.com>
2023-09-27 15:48:46 +0545

    feat🔥: simple server created using http.createServer.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,2 +1,10 @@
 const http=require('http');
 const fs=require('fs');
+
+// Server
+const server=http.createServer((req,res)=>{
+res.end('Hello from server');
+})
+server.listen(5000,'127.0.0.1',()=>{
+    console.log("Server Started");
+})
