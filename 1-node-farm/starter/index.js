7adb5b8fab44b66abf536f7e5aa075851a4fc2d9
Sandesh Ghimire <sandeshghimire202@gmail.com>
2023-09-28 10:56:30 +0545

    🔥Build simple routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,29 @@
 const http=require('http');
+const url=require('url');
 const fs=require('fs');
+
+
 
 // Server
 const server=http.createServer((req,res)=>{
-res.end('Hello from server');
+
+const pathname=req.url;
+if(pathname==='/'||pathname==='/overview'){
+    res.end("I am in Overview page");
+}else if(pathname==='/product'){
+    res.end("I am in Product page");
+}else{
+    res.writeHead(404,{
+        'content-type':'text/html',
+        'my-own-header':'hello-world',
+    })
+    res.end('<h1>Page Not Found</h1>')
+}
+console.log(pathname);
+
+
+
+
 })
 server.listen(5000,'127.0.0.1',()=>{
     console.log("Server Started");
